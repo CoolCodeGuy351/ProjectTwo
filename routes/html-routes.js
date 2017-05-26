0a02fe96feb0983dbaad337cb3b5a82e4d5bdfc5
Josh Benloulou <jbenloul@gmail.com>
2017-05-26 16:42:28 -0700

    updated api route for most popular and most recent

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,23 +6,37 @@
 // =============================================================
 var path = require("path");
 var db = require("../models");
+var apiRoutes = require('./api-routes.js')
 
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.render("homepage", {});
-  });
+        // index route loads view.html
+        app.get("/", function(req, res) {
+            res.render("userAuth", {});
+        });
 
-  app.get("/category", function(req, res) {
-    res.render("category", {});
-  });
+        app.get("/home", function(req, res) {
+            res.render("homepage", {});
+        });
 
-
-}
-
+        app.get("/category/:threeCategories", function(req, res) {
+                db.Summary.findAll({
+                    where: {
+                        category: req.params.threeCategories
+                    }
+                }).then(function(summary) {
+                  apiRoutes.findCountAll(req, res).then(function (data) {
+                    res.render("category", {
+                        catName: req.params.threeCategories,
+                        top4: data,
+                        categoryNameData: summary
+                    });
+                  })
+                });
+            });
+        }
 
 
