35e6d2b798069b3b747cd988eb4e819b9560fa05
Josh Benloulou <jbenloul@gmail.com>
2017-05-26 12:10:46 -0700

    incorporated auto0 landing page into handlebars and updated file structure

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ app.use(express.static("./public"));
 // Routes =============================================================
 
 require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(function() {
