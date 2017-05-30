b7d1b95e854995234ab21ce815cefbf8de62c3a3
Josh Benloulou <jbenloul@gmail.com>
2017-05-29 18:05:01 -0700

    made some changes to the api routes, added if/else for route category name, made a prelim login route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,23 +10,25 @@ var db = require("../models");
 
 // Routes
 // =============================================================
+//                  FUNCTIONS
+// =============================================================
 module.exports = {
     findCountAll: function(req, res, next) {
         return db.Summary.findAndCountAll({
-                include: [{
-                    model: Category,
-                    required: true,
-                    attributes: [
-                        [db.sequelize.fn('COUNT', sequelize.col('title'), 'count')]
-                    ]
-                }],
-                group: ['title'],
-                orderBy: "count DESC",
-                limit: 4
-            })
+            include: [{
+                model: Category,
+                required: true,
+                attributes: [
+                    [db.sequelize.fn('COUNT', sequelize.col('title'), 'count')]
+                ]
+            }],
+            group: ['title'],
+            orderBy: "count DESC",
+            limit: 4
+        })
     },
-    search: function(){
-        return  db.Summary.findAll({
+    search: function() {
+        return db.Summary.findAll({
                 where: {
                     title: req.body.search
                 }
@@ -37,127 +39,154 @@ module.exports = {
 
     }
 
+    // =============================================================
+    //                  ROUTES
+    // =============================================================
 }
 module.exports = function(app) {
 
-    // Get route for returning summaries of a specific category
+        // Get route for returning summaries of a specific category
+        app.get("/category/:category", function(req, res) {
+            db.Summary.findAll({
+                    where: {
+                        category: req.params.category
+                    }
+                })
+                .then(function() {
+                    res.redirect('/category');
+                });
+        });
 
-    app.get("/category/:category", function(req, res) {
-      db.Summary.findAll({
-        where: {
-          category: req.params.category
-        }
-      })
-      .then(function() {
-        res.redirect('/category');
-      });
-    });
+        // NEED TO TEST THIS OUT
+        app.get("/category/top12/:category", function(req, res) {
+            db.Summary.findAndCountAll({
+                    include: [{
+                        model: Category,
+                        required: true,
+                        attributes: [
+                            [db.sequelize.fn('COUNT', sequelize.col('title'), 'count')]
+                        ]
+                    }],
+                    group: ['title'],
+                    orderBy: "count DESC",
+                    limit: 12
+                })
+                .then(function() {
+                    res.redirect('/category');
+                });
+        });
+        // Get route for retrieving a single title
+        //Need to put in object for render as second object
+        app.get("/search", function(req, res) {
+            db.Summary.findAll({
+                    where: {
+                        title: req.body.search
+                    }
+                })
+                .then(function() {
+                    res.redirect('/category');
+                });
+        });
+        // POST route for saving a new post
+        app.post("/post", function(req, res) {
+            console.log(req.body);
+            db.Summary.create({
+                    title: req.body.title,
+                    summary: req.body.summary
+                })
+                .then(function() {
+                    res.redirect('/category');
+                });
+        });
+        // DELETE route for deleting posts
+        app.delete("/delete/posts/:id", function(req, res) {
+            db.Summary.destroy({
+                    where: {
+                        id: req.params.id
+                    }
+                })
+                .then(function() {
+                    res.redirect('/home');
+                });
+        });
 
-    // NEED TO TEST THIS OUT
-    app.get("/category/top12/:category", function(req, res) {
-        db.Summary.findAndCountAll({
-                include: [{
-                    model: Category,
-                    required: true,
-                    attributes: [
-                        [db.sequelize.fn('COUNT', sequelize.col('title'), 'count')]
-                    ]
-                }],
-                group: ['title'],
-                orderBy: "count DESC",
-                limit: 12
-            })
-            .then(function() {
-                res.redirect('/category');
-            });
-    });
+        // PUT route for updating posts
+        app.put("/posts", function(req, res) {
+            db.Summary.update(req.body, {
+                    where: {
+                        id: req.body.id
+                    }
+                })
+                .then(function() {
+                    res.redirect('/category');
+                });
+        });
 
 
-    // Get route for retrieving a single title
-    //Need to put in object for render as second object
-    app.get("/search", function(req, res) {
-        db.Summary.findAll({
-                where: {
-                    title: req.body.search
+        // =============================================================
+        //                  LOGIN
+        // =============================================================
+
+        // on sign in. going to have to create a link from the login that links to the homepage on the submit click
+        // make an if/else statement to send the user an error message or page if they mess up on the sign in.
+        app.get('/loginPage', function(req, res) {
+                    db.Author.findOne({
+                        where: {
+                            username: req.body.username,
+                            password: req.body.password
+                        }.then(function() {
+                            res.render("homepage", {});
+                        })
+                    });
                 }
-            })
-            .then(function() {
-                res.redirect('/category');
-            });
-    });
-
-    // POST route for saving a new post
-    //DONE
-    app.post("/post", function(req, res) {
-        console.log(req.body);
-        db.Summary.create({
-                title: req.body.title,
-                summary: req.body.summary
-            })
-            .then(function() {
-                res.redirect('/category');
-            });
-    });
-
-    // DELETE route for deleting posts
-    //DONE
-    app.delete("/delete/posts/:id", function(req, res) {
-        db.Summary.destroy({
-                where: {
-                    id: req.params.id
-                }
-            })
-            .then(function() {
-                res.redirect('/home');
-            });
-    });
-
-    // PUT route for updating posts
-    //DONE
-    app.put("/posts", function(req, res) {
-        db.Summary.update(req.body, {
-                where: {
-                    id: req.body.id
-                }
-            })
-            .then(function() {
-                res.redirect('/category');
-            });
-    });
-}
 
 
 
 
 
 
-//on submit button
-
-  // var syllables = [],
-  // var syllableCount = 0
-  // for (var i = 0; i < syllables.length; i++) {
-  // var queryURL = "https://wordsapiv1.p.mashape.com/words/" + syllables[i] + "/syllables";
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  //   }).done(function (syllables) {
-  //     console.log(syllables.syllables.count)
-  //     syllableCount += syllables.syllables.count
-  //   })
-
-  //   if (syllableCount === 14) {
-  //      db.Summary.create({
-  //               title: req.body.title,
-  //               summary: req.body.summary
-  //           })
-  //           .then(function() {
-  //               res.redirect('/category');
-  //           });
-  //   } else {
-    // console.log("This isn't 14 syllables, try again")
-  // }
-
- // }
 
 
-  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //on submit button
+
+                // var syllables = [],
+                // var syllableCount = 0
+                // for (var i = 0; i < syllables.length; i++) {
+                // var queryURL = "https://wordsapiv1.p.mashape.com/words/" + syllables[i] + "/syllables";
+                // $.ajax({
+                //   url: queryURL,
+                //   method: "GET"
+                //   }).done(function (syllables) {
+                //     console.log(syllables.syllables.count)
+                //     syllableCount += syllables.syllables.count
+                //   })
+
+                //   if (syllableCount === 14) {
+                //      db.Summary.create({
+                //               title: req.body.title,
+                //               summary: req.body.summary
+                //           })
+                //           .then(function() {
+                //               res.redirect('/category');
+                //           });
+                //   } else {
+                // console.log("This isn't 14 syllables, try again")
+                // }
+
+                // }
+
+
+                // }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,7 +6,7 @@
 // =============================================================
 var path = require("path");
 var db = require("../models");
-var passport = require('passport')
+// var passport = require('passport')
 var apiRoutes = require('./api-routes.js')
 
 
@@ -14,30 +14,62 @@ var apiRoutes = require('./api-routes.js')
 // =============================================================
 module.exports = function(app) {
 
-    // index route loads view.html
-    app.get("/", function(req, res) {
-        res.render("userAuth", {});
-    });
-
-    app.get("/home", function(req, res) {
-        res.render("homepage", {});
-    });
-
-    app.get("/category/:threeCategories", function(req, res) {
-        db.Summary.findAll({
-            where: {
-                category: req.params.threeCategories
-            }
-        }).then(function(summary) {
-            apiRoutes.findCountAll(req, res).then(function(data) {
-                res.render("category", {
-                    catName: req.params.threeCategories,
-                    top4: data,
-                    categoryNameData: summary
-                });
-            })
+        // index route loads view.html
+        app.get("/", function(req, res) {
+            res.render("userAuth", {});
         });
-    });
 
-}
+        app.get("/home", function(req, res) {
+            res.render("homepage", {});
+        });
 
+        app.get("/category/:catName", function(req, res) {
+            res.render("category", {});
+        });
+
+
+        // app.get("/category/:threeCategories", function(req, res) {
+        //     db.Summary.findAll({
+        //         where: {
+        //             categoryId: req.params.threeCategories
+        //         }
+        //     }).then(function(summary) {
+        //         apiRoutes.findCountAll(req, res).then(function(data) {
+        //             res.render("category", {
+        //                 catName: req.params.threeCategories,
+        //                 top4: data,
+        //                 categoryNameData: summary
+        //             });
+        //         })
+        //     });
+        // });
+
+
+// How to get the 3 categories to match up with the categoryId in the database, since the column will habe a number and not a value.
+        // app.get("/category/:threeCategories", function(req, res) {
+        //         if (req.params.threeCategories === "books") {
+        //             db.Summary.findAll({
+        //                 //         where: {
+        //                 //             categoryId: 1
+        //                 //         }
+        //             })
+        //         } else if (req.params.threeCategories === "movies") {
+        //             db.Summary.findAll({
+        //                 //         where: {
+        //                 //             categoryId: 2
+        //                 //         }
+        //             })
+        //         } else if (req.params.threeCategories === "books") {
+        //             db.Summary.findAll({
+        //                 //         where: {
+        //                 //             categoryId: 3
+        //                 //         }
+        //             })
+        //         } else {
+
+        //         }
+        //     }
+
+
+
+        }
